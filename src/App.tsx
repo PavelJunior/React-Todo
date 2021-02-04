9428ebfe8f99247fa26ae99a4fc9886dda5dcb6b
PavelJunior <pavelpyshpavel@gmail.com>
2021-02-03 22:52:27 -0800

    Created .prettierrc and TodoListItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import { TodoListItem } from './components/TodoListItem';
+
+const todos: Array<Todo> = [
+  {
+    text: 'walk the dog',
+    complete: true,
+  },
+  {
+    text: 'do dishes',
+    complete: false,
+  },
+  {
+    text: 'write an app',
+    complete: false,
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+  return <TodoListItem todo={todos[0]} />;
 }
 
 export default App;
