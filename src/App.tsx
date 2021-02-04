a3d1ec810215af0880abd29ea07c1d2809b99ceb
PavelJunior <pavelpyshpavel@gmail.com>
2021-02-03 23:23:09 -0800

    Created TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TodoListItem } from './components/TodoListItem';
+import { TodoList } from './components/TodoList';
 
 const initialTodos: Array<Todo> = [
   {
@@ -32,13 +32,7 @@ function App() {
     setTodos(newTodos);
   };
 
-  return (
-    <>
-      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
-      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
-      <TodoListItem todo={todos[2]} toggleTodo={toggleTodo} />
-    </>
-  );
+  return <TodoList toggleTodo={toggleTodo} todos={todos} />;
 }
 
 export default App;
